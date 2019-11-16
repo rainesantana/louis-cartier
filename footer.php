6ff7d375405b93c3bba85d498aed22782a83c524
Raine <rainesgoncalves@gmail.com>
2019-11-16 08:20:19 -0300

    footer projeto

---
+++ footer.php
@@ -0,0 +1,62 @@
+<footer class="footer bg-light section">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md">
+                    <div class="footer-column mb-4">
+                        <h2>Louis Cartier</h2>
+                        <ul class="footer-social list-unstyled float-md-left float-lft mt-5">
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="footer-column mb-4 ml-md-5">
+                        <h2>Menu</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-2 d-block">Home</a></li>
+                            <li><a href="#" class="py-2 d-block">Coleção</a></li>
+                            <li><a href="#" class="py-2 d-block">Contato</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="footer-column mb-4">
+                        <h2>Central de atendimento</h2>
+                        <div class="d-flex">
+                            <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
+                                <li><a href="#" class="py-2 d-block">Sobre nós</a></li>
+                                <li><a href="#" class="py-2 d-block">Trocas e devoluções</a></li>
+                                <li><a href="#" class="py-2 d-block">Envio e entrega</a></li>
+                                <li><a href="#" class="py-2 d-block">Compra segura</a></li>
+                            </ul>
+                            <ul class="list-unstyled">
+                                <li><a href="#" class="py-2 d-block">FAQ</a></li>
+                                <li><a href="#" class="py-2 d-block">Contato</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="footer-column mb-4">
+                        <h2>Dúvidas?</h2>
+                        <div class="block-23 mb-3">
+                            <ul>
+                                <li><i class="fa fa-map-pin mr-2"></i><span class="text">Av. Dr. Cardoso de Melo, 90 - Vila Olímpia, São Paulo - SP,</span></li>
+                                <li><a href="#"><i class="fa fa-whatsapp mr-2"></i><span class="text">(00) 00000-0000</span></a></li>
+                                <li><a href="#"><i class="fa fa-envelope mr-2"></i><span class="text">contato@louiuscartier.com.br</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p>
+                        Copyright &copy; 2019
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
