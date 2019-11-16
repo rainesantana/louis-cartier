343960ec51eb231f933d87a82bedf676c539109d
Raine <rainesgoncalves@gmail.com>
2019-11-16 08:21:40 -0300

    home projeto

---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<?php include_once "head.php"; ?>
+
+<body>
+    <?php include_once "header.php"; ?>
+
+    <div class="go-to-here"></div>
+
+    <section class="section product">
+        <div class="container">
+            <div class="row justify-content-center mb-3 pb-3">
+                <div class="col-md-12 heading-section text-center">
+                    <h1 class="mb-4">Lançamentos</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="product-slider owl-carousel">
+                        <div class="item">
+                            <div class="product">
+                                <a href="#" class="img-prod">
+                                    <img class="img-fluid" src="assets/images/produtos/relogio_1.jpg" alt="Neque porro quisquam est qui dolorem ">
+                                    <span class="sale">30%</span>
+                                </a>
+                                <div class="text pt-3 px-3">
+                                    <h3><a href="#">Neque porro quisquam est qui dolorem </a></h3>
+                                    <div class="d-flex">
+                                        <div class="pricing">
+                                            <p class="price"><span class="mr-2 price">R$ 20.000,00</span><span class="price-sale">R$14.000,00</span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="product">
+                                <a href="#" class="img-prod">
+                                    <img class="img-fluid" src="assets/images/produtos/relogio_2.jpg" alt="Neque porro quisquam est qui dolorem ">
+                                </a>
+                                <div class="text pt-3 px-3">
+                                    <h3><a href="#">Neque porro quisquam est qui dolorem </a></h3>
+                                    <div class="d-flex">
+                                        <div class="pricing">
+                                            <p class="price"><span class="price-sale">R$2.500,00</span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="product">
+                                <a href="#" class="img-prod">
+                                    <img class="img-fluid" src="assets/images/produtos/relogio_3.jpg" alt="Neque porro quisquam est qui dolorem ">
+                                </a>
+                                <div class="text pt-3 px-3">
+                                    <h3><a href="#">Neque porro quisquam est qui dolorem </a></h3>
+                                    <div class="d-flex">
+                                        <div class="pricing">
+                                            <p class="price"><span class="price-sale">R$9.000,00</span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="product">
+                                <a href="#" class="img-prod">
+                                    <img class="img-fluid" src="assets/images/produtos/relogio_4.jpg" alt="Neque porro quisquam est qui dolorem ">
+                                    <span class="sale">10%</span>
+                                </a>
+                                <div class="text pt-3 px-3">
+                                    <h3><a href="#">Neque porro quisquam est qui dolorem </a></h3>
+                                    <div class="d-flex">
+                                        <div class="pricing">
+                                            <p class="price"><span class="mr-2 price">R$ 14.000,00</span><span class="price-sale">R$9.800,00</span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="product">
+                                <a href="#" class="img-prod">
+                                    <img class="img-fluid" src="assets/images/produtos/relogio_2.jpg" alt="Neque porro quisquam est qui dolorem ">
+                                </a>
+                                <div class="text pt-3 px-3">
+                                    <h3><a href="#">Neque porro quisquam est qui dolorem </a></h3>
+                                    <div class="d-flex">
+                                        <div class="pricing">
+                                            <p class="price"><span class="price-sale">R$10.000,00</span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="product">
+                                <a href="#" class="img-prod">
+                                    <img class="img-fluid" src="assets/images/produtos/relogio_1.jpg" alt="Neque porro quisquam est qui dolorem ">
+                                </a>
+                                <div class="text pt-3 px-3">
+                                    <h3><a href="#">Neque porro quisquam est qui dolorem </a></h3>
+                                    <div class="d-flex">
+                                        <div class="pricing">
+                                            <p class="price"><span class="price-sale">R$10.000,00</span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section no-pt bg-light pb-9">
+        <div class="container">
+            <div class="row">
+                <figure class="col-md-5 p-md-5 img img-featured d-flex justify-content-center align-items-center">
+                    <img src="assets/images/destaque.jpg" alt="">
+                </figure>
+                <div class="col-md-7 py-5 wrap-about pb-md-5">
+                    <div class="heading-section-bold mb-5 mt-md-5">
+                        <div class="ml-md-0">
+                            <h2 class="mb-4">Tenha <br><span>relógios excluivos</span></h2>
+                        </div>
+                    </div>
+                    <div class="pb-md-5">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ex a nulla venenatis rutrum. In porttitor iaculis est id ornare. Fusce malesuada metus non tortor porttitor efficitur. Donec eget accumsan est, eget ultricies odio.
+                            Proin tempor massa eget nulla hendrerit pretium. Donec iaculis justo id leo blandit, in pharetra dolor iaculis. Nullam felis nunc, maximus elementum nulla quis, pulvinar convallis elit. Sed vitae orci ac ipsum cursus egestas.
+                            Etiam congue nec eros in eleifend. Nunc hendrerit rutrum convallis. Donec ullamcorper nisl ut est tincidunt semper.</p>
+                        <p>In in arcu ipsum. Suspendisse est nulla, molestie nec fermentum ac, luctus non odio. In vitae lacus pretium sapien volutpat placerat. Fusce sodales ultrices ipsum id hendrerit. Vestibulum consequat dolor et sem vehicula, elementum
+                            semper purus pharetra. Vestibulum volutpat rutrum commodo. Ut non nulla placerat, ultricies metus ut, laoreet justo. In molestie velit est, eget luctus metus porttitor in. Nam quis tellus dolor. Quisque porta et metus ut auctor.
+                            Cras quis euismod quam. Nullam mollis porttitor mauris, id sagittis ex convallis vel. Praesent dignissim, dolor ut suscipit imperdiet, sapien dolor dictum enim, in cursus turpis lacus et sem. Phasellus id efficitur felis. Pellentesque
+                            non purus quam.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+   <?php @include_once "footer.php"; ?>
+
+    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="assets/js/jquery.easing.1.3.js"></script>
+    <script src="assets/js/jquery.waypoints.min.js"></script>
+    <script src="assets/js/jquery.stellar.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/scrollax.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
