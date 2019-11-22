a2bbfe03781af2d1cc6f409db958342ca26e253e
Marcos Denilson <marcosbirino66@gmail.com>
2019-11-22 00:43:08 -0300

    Adicionado o Formulario de cadastro linkado ao botao cadastre-se

---
+++ cadastro.php
@@ -0,0 +1,73 @@
+<section>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  
+<link rel="stylesheet" href="../assets/css/cadastro.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<form action="cadastro.php" method="POST">
+<div class="container">
+<div class="form-inline col-12 text-center  my-5" action="" method="POST">  
+    <h1>Seja um cliente Louis-cartier</h1>
+    <h3 class="text-info">Cadastre-se</h3>
+
+ <div class="row">
+          
+        <div class="form-group">
+            <label for="nomeCompleto" class="text-success">Nome:</label><br>
+            <input type="text" class="form-control" id="nomeCompleto" class="form-control" placeholder="Insira seu nome completo" required><br>
+        </div>  
+       
+ </div>  
+
+ <div class="row">
+        <div class="form-group">
+            <label for="Email" class="text-success">E-mail:</label> <br>
+            <input type="text" class="form-control" id="Email" class="form-control" placeholder="Insira um e-mail alternativo" required><br>
+        </div>      
+
+ </div>  
+
+ <div class="row">
+
+         <div class="form-group">
+            <label for="CPF" class="text-success">CPF:</label><br>
+            <input type="text" class="form-control" id="CPF" class="form-control" placeholder="Insira seu CPF" required><br>
+        </div>
+</div> 
+
+<div class="row">
+            <div class="form-group">
+            <label for="Telefone" class="text-success">Telefone:</label><br>
+            <input type="text" class="form-control" id="Telefone" class="form-control" placeholder="Insira Telefone(celular)" required><br>
+        </div>            
+
+</div>     
+
+<div class="row">       
+        <div class="form-group">
+            <label for="senha" class="text-success">Senha:</label><br>
+            <input type="password" class="form-control" id="senha" class="form-control" required><br>
+        </div>
+</div> 
+       
+<div class="row"> 
+        <div class="form-group">
+            <label for="Senha" class="text-success">Insira novamente:</label><br>
+            <input type="password" class="form-control" name="Senha" id="Senha" class="form-control" required><br>
+        </div>
+        
+</div> 
+
+        
+<br>
+
+    <button type="submit" class="btn btn-success">Confirmar Cadastro</button>
+    <button  type="reset" class="btn btn-primary">Limpar Cadastro</button>
+   
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   </form>
+ </div>
+</section>
