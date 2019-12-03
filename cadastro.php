2a4587488b7a36d140a1999a7884ad90f6c5a953
MarcosDenilson <38533187+MarcosDenilson@users.noreply.github.com>
2019-12-03 01:12:43 -0300

    Alteração na tela de cadastro,mais alterações futuras no leyout

--- cadastro.php
+++ cadastro.php
@@ -1,73 +1,88 @@
-<section>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  
-<link rel="stylesheet" href="../assets/css/cadastro.css">
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<form action="cadastro.php" method="POST">
-<div class="container">
-<div class="form-inline col-12 text-center  my-5" action="" method="POST">  
-    <h1>Seja um cliente Louis-cartier</h1>
-    <h3 class="text-info">Cadastre-se</h3>
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once ("inc/head.php"); ?>
 
- <div class="row">
-          
-        <div class="form-group">
-            <label for="nomeCompleto" class="text-success">Nome:</label><br>
-            <input type="text" class="form-control" id="nomeCompleto" class="form-control" placeholder="Insira seu nome completo" required><br>
-        </div>  
-       
- </div>  
 
- <div class="row">
-        <div class="form-group">
-            <label for="Email" class="text-success">E-mail:</label> <br>
-            <input type="text" class="form-control" id="Email" class="form-control" placeholder="Insira um e-mail alternativo" required><br>
-        </div>      
 
- </div>  
+<body class="bg-dark"> 
+<?php require_once ("inc/header.php"); ?>
 
- <div class="row">
 
-         <div class="form-group">
-            <label for="CPF" class="text-success">CPF:</label><br>
-            <input type="text" class="form-control" id="CPF" class="form-control" placeholder="Insira seu CPF" required><br>
-        </div>
-</div> 
 
-<div class="row">
-            <div class="form-group">
-            <label for="Telefone" class="text-success">Telefone:</label><br>
-            <input type="text" class="form-control" id="Telefone" class="form-control" placeholder="Insira Telefone(celular)" required><br>
-        </div>            
+<div class="container text-center">
 
-</div>     
+<form  method="POST" action="" name="">
 
-<div class="row">       
-        <div class="form-group">
-            <label for="senha" class="text-success">Senha:</label><br>
-            <input type="password" class="form-control" id="senha" class="form-control" required><br>
-        </div>
-</div> 
-       
-<div class="row"> 
-        <div class="form-group">
-            <label for="Senha" class="text-success">Insira novamente:</label><br>
-            <input type="password" class="form-control" name="Senha" id="Senha" class="form-control" required><br>
-        </div>
-        
-</div> 
+    <section class="mt-5 container text-center ">
 
-        
-<br>
+    <h2 class="text-white">Cadastre-se</h2>
+    <p class="text-white">Seja um cliente Louis-cartier</p>
 
-    <button type="submit" class="btn btn-success">Confirmar Cadastro</button>
-    <button  type="reset" class="btn btn-primary">Limpar Cadastro</button>
+    <br><br>
    
+        <div class="row">
+ 
+                    <div class="col">
+                        <label for="nomeCompleto" class="text-white">Nome:</label>
+                        <input type="text" class="form-control" id="nomeCompleto" placeholder="Insira seu nome completo" required>
+                    </div>
+                        <div class="col">
+                        <label for="Email" class="text-white">E-mail:</label>
+                        <input type="text" class="form-control" id="Email"  placeholder="Insira um e-mail alternativo" required><br>
+                    </div>
 
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-   </form>
- </div>
+        </div>
+        <br>
+        <div class="row">
+                    <div class="col">
+                        <label for="CPF" class="text-white">CPF:</label>
+                        <input type="text" class="form-control" id="CPF"  placeholder="Insira seu CPF" required>
+                    </div>
+
+                    <div class="col">
+                        <label for="Telefone" class="text-white">Telefone:</label>
+                        <input type="text" class="form-control" id="Telefone"  placeholder="Insira Telefone(celular)" required>
+                    </div>
+        </div>
+        <br>
+        <div class="row">
+                   
+                <div class="col">
+                    <label for="senha" class="text-white">Senha:</label>
+                    <input type="password" class="form-control" id="senha"  placeholder="Informe senha com letra e numero" required>
+                </div>
+                <div class="col">
+                    <label for="Senha" class="text-white">Insira novamente:</label> 
+                    <input type="password" class="form-control" name="Senha" id="Senha"  placeholder="Informe senha novamente" required>
+                </div>
+
+        </div>
+
+    <br><br><br>
+    
+   
+    <button type="submit" class="btn btn-success">confirmar cadastro</button>
+      
+    <button  type="reset" class="btn btn-primary">cadastrar novamente</button>
+
+     
+   
+    
+    <br><br><br>
+
+    <?php @require_once ("inc/footer.php"); ?>
+
 </section>
+   </form>
+  </div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
